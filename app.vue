9b9dd6b9d96e0f928cf91f5f8d8621bd4c51a042
MatteoDuco <matteoducoli.is@gmail.com>
2025-03-03 12:10:57 +0100

    feat: SEO tags

--- app.vue
+++ app.vue
@@ -1,9 +1,33 @@
-<script lang="ts" setup>
-const { config } = useAppConfig()
-
-useHead({
-  title: config.name,
-  titleTemplate: `%s - ${config.name}`,
+<script setup lang="ts">
+useSeoMeta({
+  // should be page
+  title: 'Matteo Ducoli',
+  ogTitle: 'Matteo Ducoli',
+  twitterTitle: 'Matteo Ducoli',
+  description: 'Matteo Ducoli, full stack developer and software engineer',
+  ogDescription: 'Matteo Ducoli, full stack developer and software engineer',
+  twitterDescription: 'Matteo Ducoli, full stack developer and software engineer',
+  ogUrl: 'https://ducolimatteo.it',
+  ogImage: {
+    url: 'https://ducolimatteo.it/og-image.jpeg',
+    width: 800,
+    height: 800,
+    alt: 'Matteo Ducoli',
+  },
+  ogImageType: 'image/jpeg',
+  ogImageSecureUrl: 'https://ducolimatteo.it/og-image.jpeg',
+  ogImageWidth: 800,
+  ogImageHeight: 800,
+  twitterImage: {
+    url: 'https://ducolimatteo.it/og-image.jpeg',
+    width: 800,
+    height: 800,
+    alt: 'Matteo Ducoli',
+  },
+  // global
+  ogLocale: 'en_US',
+  ogType: 'website',
+  ogSiteName: 'Matteo Ducoli',
 })
 </script>
 
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,18 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   eslint: { config: { standalone: false } },
 
-  modules: [
-    '@nuxt/eslint',
-    '@nuxt/fonts',
-    '@nuxtjs/tailwindcss',
-  ],
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      noscript: [
+        // <noscript>JavaScript is required</noscript>
+        { children: 'JavaScript is required' },
+      ],
+    },
+  },
+
+  modules: ['@nuxt/eslint', '@nuxt/fonts', '@nuxtjs/tailwindcss'],
 
   ssr: false,
   compatibilityDate: '2025-01-26',
